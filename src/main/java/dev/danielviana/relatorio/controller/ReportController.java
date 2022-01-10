e6993677249cd4b2ea0cfc8cad22271c7f39ca2d
Daniel Lopes <viana.daniel02@gmail.com>
2022-01-09 22:29:27 -0300

    feat: relatório do tipo CSV (super-csv).

--- src/main/java/dev/danielviana/relatorio/controller/ReportController.java
+++ src/main/java/dev/danielviana/relatorio/controller/ReportController.java
@@ -1,8 +1,12 @@
 package dev.danielviana.relatorio.controller;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -13,10 +17,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import dev.danielviana.PDFUtils;
 import dev.danielviana.relatorio.model.City;
 import dev.danielviana.relatorio.model.ReportChart;
+import dev.danielviana.relatorio.model.State;
 import dev.danielviana.relatorio.services.CityService;
 import dev.danielviana.relatorio.services.StateService;
 
@@ -67,6 +75,27 @@ public class ReportController {
         ByteArrayInputStream pdf = PDFUtils.generatePDf(list);
 
         return ResponseEntity.ok().header("Content-Disposition", "inline; filename=report.pdf").contentType(MediaType.APPLICATION_PDF).body(new InputStreamResource(pdf));
+    }
+
+    @GetMapping("/csv")
+    public void csv(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setHeader("Content-Disposition", "attachment; filename=planilha.csv");
+
+        String [] header = {"id", "name"};
+
+        ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+        csvBeanWriter.writeHeader(header);
+
+        List<State> states = (List<State>) stateService.findAll();
+
+        for(State state : states){
+            csvBeanWriter.write(state, header);
+        }
+
+        csvBeanWriter.close();
+
+
     }
 
 }
